1b4d0e4f9119ce9a8edd6b6dd22c7ae631d949c1
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-11 02:32:05 -0700

    master: add workbench

---
+++ output/relations.sql
@@ -0,0 +1,31 @@
+booktown=# \d
+booktown=# \d
+                    List of relations
+ Schema |         Name          |   Type   |    Owner     
+--------+-----------------------+----------+--------------
+ public | alternate_stock       | table    | dreammachine
+ public | authors               | table    | dreammachine
+ public | book_backup           | table    | dreammachine
+ public | book_ids              | sequence | dreammachine
+ public | book_queue            | table    | dreammachine
+ public | books                 | table    | dreammachine
+ public | customers             | table    | dreammachine
+ public | daily_inventory       | table    | dreammachine
+ public | distinguished_authors | table    | dreammachine
+ public | editions              | table    | dreammachine
+ public | employees             | table    | dreammachine
+ public | favorite_authors      | table    | dreammachine
+ public | favorite_books        | table    | dreammachine
+ public | money_example         | table    | dreammachine
+ public | my_list               | table    | dreammachine
+ public | numeric_values        | table    | dreammachine
+ public | publishers            | table    | dreammachine
+ public | schedules             | table    | dreammachine
+ public | shipments             | table    | dreammachine
+ public | states                | table    | dreammachine
+ public | stock                 | table    | dreammachine
+ public | stock_backup          | table    | dreammachine
+ public | subject_ids           | sequence | dreammachine
+ public | subjects              | table    | dreammachine
+ public | text_sorting          | table    | dreammachine
+(25 rows)
---
+++ workbench.sql
@@ -0,0 +1,5 @@
+-- Order
+-- Find all subjects sorted by subject
+SELECT * FROM subjects;
+
+-- Find all subjects sorted by location
